8ecde936a465a45fb2daf8e19a9727cdd1a13ba4
Weber Chang <s0975247623@gmail.com>
2024-12-13 16:58:52 +0800

    More blending ouob

--- models/Blending/blending.py
+++ models/Blending/blending.py
@@ -1,44 +1,23 @@
 import csv
+import math
+import numpy as np
 
 res = [0] * 6185
+model_cnt = 6
+acc = [0.55422, 0.58779, 0.58166, 0.55390, 0.57779, 0.58553]
+name = ["./gaussian_SVM_1.csv", "./blending.csv", "./xgboost.csv", "./lr.csv", "./random-forest-optimized.csv", "./dnn-recovered.csv"]
 
-with open("./gaussian_SVM_1.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
+def w(x):
+    return x
 
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.55422 * (1 if rows[i][1] == "True" else -1)
+for j in range(model_cnt):
+    with open(name[j], "r", newline='') as f1:
+        rows = list(csv.reader(f1))
 
-with open("./mean_shift_predictions.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
+        for i in range(1, len(rows)):
+            res[i - 1] += w(acc[j]) * (1 if rows[i][1] == "True" else -1)
 
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.52969 * (1 if rows[i][1] == "True" else -1)
-
-with open("./adaboost.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
-
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.56036 * (1 if rows[i][1] == "True" else -1)
-
-with open("./lr.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
-
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.55390 * (1 if rows[i][1] == "True" else -1)
-
-with open("./random-forest-optimized.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
-
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.57779 * (1 if rows[i][1] == "True" else -1)
-
-with open("./dnn-1.csv", "r", newline='') as f1:
-    rows = list(csv.reader(f1))
-
-    for i in range(1, len(rows)):
-        res[i - 1] += 0.52001 * (1 if rows[i][1] == "True" else -1)
-
-with open("./blending.csv", "w", newline='') as f:
+with open("./blending_res.csv", "w", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["id", "home_team_win"])
     for i in range(0, len(res)):
