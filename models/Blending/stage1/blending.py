84a1062650c5a73c5d4ad10be8dc3fb7559ddff0
Weber Chang <s0975247623@gmail.com>
2024-12-15 16:28:34 +0800

    Final Blending with expoenetial weight

--- models/Blending/stage1/blending.py
+++ models/Blending/stage1/blending.py
@@ -4,11 +4,12 @@ import numpy as np
 
 res = [0] * 6185
 model_cnt = 4
-acc = [0.55422, 0.58166, 0.57779, 0.58553, 0.52969, 0.55390]
-name = ["./gaussian_SVM_1.csv", "./xgboost.csv", "./random-forest-optimized.csv", "./dnn-recovered.csv", "./mean_shift_predictions.csv", "./lr.csv"]
+acc = [0.55422, 0.58166, 0.57779, 0.58650, 0.52969, 0.55390]
+ranking = [3, 5, 4, 6, 1, 2]
+name = ["./gaussian_SVM_1.csv", "./xgboost.csv", "./random-forest-optimized.csv", "./dnn_model_submissions.csv", "./mean_shift_predictions.csv", "./lr.csv"]
 
 def w(x):
-    return x
+    return 3.85 ** 3.85 ** 3.85 ** x
 
 for j in range(model_cnt):
     with open(name[j], "r", newline='') as f1:
@@ -17,7 +18,7 @@ for j in range(model_cnt):
         for i in range(1, len(rows)):
             res[i - 1] += w(acc[j]) * (1 if rows[i][1] == "True" else -1)
 
-with open("./blending_res.csv", "w", newline='') as f:
+with open("./blending_res_2.csv", "w", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["id", "home_team_win"])
     for i in range(0, len(res)):
--- models/Blending/stage2/blending.py
+++ models/Blending/stage2/blending.py
@@ -8,7 +8,7 @@ acc = [0.58388, 0.57724, 0.57308, 0.55481]
 name = ["./dnn-recovered_stage2.csv", "./xgboost.csv", "./rbf_svm.csv", "./lin_reg.csv"]
 
 def w(x):
-    return x
+    return 3.65 ** 3.65 ** 3.65 ** x
 
 for j in range(model_cnt):
     with open(name[j], "r", newline='') as f1:
@@ -17,7 +17,7 @@ for j in range(model_cnt):
         for i in range(1, len(rows)):
             res[i - 1] += w(acc[j]) * (1 if rows[i][1] == "True" else -1)
 
-with open("./blending_res.csv", "w", newline='') as f:
+with open("./blending_res_2.csv", "w", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["id", "home_team_win"])
     for i in range(0, len(res)):
